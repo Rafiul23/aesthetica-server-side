e7867c9cc239fbcc9a7ec484a965250115af11a8
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-20 20:05:37 +0600

    post method is working well

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const productsCollection = client.db('productsDB').collection('product');
+
+    app.post('/products', async(req, res)=>{
+        const newProduct = req.body;
+        const result = await productsCollection.insertOne(newProduct);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
