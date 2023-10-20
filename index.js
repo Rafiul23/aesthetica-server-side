c68285bea986406c33d8b3de58f92282f9920550
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-10-21 03:51:31 +0600

    fetching one product data is working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -45,6 +45,15 @@ async function run() {
         const result = await productsCollection.find(query).toArray();
         res.send(result);
     })
+
+    app.get('/product/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+    })
+
+    
 
 
     // Send a ping to confirm a successful connection
