e0167c8d9acc2e3922d1eb64d5372a45790dac59
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-11-15 19:00:35 +0600

    written a function to connect db

---
+++ config/db.js
@@ -0,0 +1,33 @@
+// config/db.js
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlof2pa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db = null;
+
+async function connectDB() {
+
+  if(db){
+    return db;
+  }
+
+  try {
+    await client.connect();
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    return db = client.db("productsDB"); 
+  } catch (err) {
+    console.error("MongoDB connection failed", err);
+    process.exit(1); // Exit if the connection fails
+  }
+}
+
+module.exports = connectDB;
